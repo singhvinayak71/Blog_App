a8d3a488c1b360131513da9400fe0b14a3fd5cb4
Vinayak <singhvinayak7111@gmail.com>
2023-08-14 17:08:08 +0530

    adding file AddBlog.jsx

---
+++ src/Pages/AddBlog.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import { Header } from "../components/Header";
+import "../styles/form.css"
+export const AddBlog =({updateData,prevData})=>{
+    const[blogData,setBlogData]=useState({
+        title:"",
+        img:"",
+        desc:"",
+        content:""
+    })
+
+    const submitData=()=>{
+        updateData([blogData,...prevData])
+    }
+    return(
+        <div id="formParent">
+            <div id="form">
+                <h1>Add Your Blog, Now!</h1>
+                <input type="text" placeholder="Enter Image Url" value={blogData.img} onChange={(e)=>{setBlogData({...blogData,img:e.target.value})}}/>
+                <input type="text" placeholder="Enter Title of your blog" value={blogData.title} onChange={(e)=>{setBlogData({...blogData,title:e.target.value})}}/>
+                <input type="text" placeholder="Type Short Description of your blog"value={blogData.desc} onChange={(e)=>{setBlogData({...blogData,desc:e.target.value})}} />
+                <textarea name="" placeholder="Type your blog" id="" cols="30" rows="10" value={blogData.content} onChange={(e)=>{setBlogData({...blogData,content:e.target.value})}}></textarea>
+                <button onClick={submitData}>Add Blog</button>
+            </div>
+        </div>
+    )
+}
