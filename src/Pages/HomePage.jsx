1a9048d688ad3a1bc4cf3457301225d59c1b3683
Vinayak <singhvinayak7111@gmail.com>
2023-08-14 17:04:27 +0530

    Home page add

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,23 +1,20 @@
-import React from 'react'
-// import { Header } from '../Components/Header'
-import BlockCard from '../Components/BlogCard';
-import '../style/homePage.css'
+import React, { useState } from "react";
+import { Header } from "../components/Header";
+import { BlogCard } from "../components/BlogCard";
+import "../styles/homepage.css"
 
-const HomePage = () => {
-  return (
-    <div id='HomePage'>
-        {/* <Header/> */}
-        <div id="blogParent">
-            <BlockCard/>
-            <BlockCard/>
-            <BlockCard/>
-            <BlockCard/>
-            <BlockCard/>
-            <BlockCard/>
+export const HomePage=({blogData})=>{
+    return(
+        <div id="HomePage">
+        
+            <div id="blogParent">
+             {
+                blogData.map((e)=>{
+                    return <BlogCard img={e.img} desc={e.desc} title={e.title}/>
+                })
+             }
+          
+            </div>
         </div>
-    </div>
-    
-  )
+    )
 }
-
-export default HomePage;
